73c127e046b23d37afd1be475dfbb1672bcb5ed9
theblindprophet <theblndprophet@gmail.com>
2018-03-28 11:19:27 -0400

    testAddAndDeleteRecipes

--- unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -17,6 +17,9 @@ public class CoffeeMakerTest extends TestCase {
 	private Recipe r3;
 	private Recipe r4;
 
+	/**
+	 * Setup of tests
+	 */
 	protected void setUp() throws Exception {
 		cm = new CoffeeMaker();
 		
@@ -59,6 +62,10 @@ public class CoffeeMakerTest extends TestCase {
 		super.setUp();
 	}
 	
+	/**
+	 * Already existed.
+	 * Adds inventory to coffee maker
+	 */
 	public void testAddInventory() {
 		try {
 			cm.addInventory("4","7","0","9");
@@ -67,6 +74,10 @@ public class CoffeeMakerTest extends TestCase {
 		}
 	}
 	
+	/**
+	 * Already existed.
+	 * Adds inventory to coffee maker with invalid inputs
+	 */
 	public void testAddInventoryException() {
 		try {
 			cm.addInventory("4", "-1", "asdf", "3");
@@ -76,9 +87,42 @@ public class CoffeeMakerTest extends TestCase {
 		}
 	}
 	
+	/**
+	 * Already existed.
+	 * Returns change of purchase
+	 */
 	public void testMakeCoffee() {
 		cm.addRecipe(r1);
 		assertEquals(25, cm.makeCoffee(0, 75));
+	}
+	
+	/**
+	 * Add and delete recipes
+	 * 
+	 * This revealed that once a recipe is removed from the recipe book, another
+	 * recipe cannot be added to the same spot again. So if you add 2 recipes, remove then 2nd, add a 3rd...
+	 * that recipe will be put into the 3rd spot instead of the second.
+	 * This was because when a recipes was deleted the index is occupied was filled with a new blank
+	 * recipe instead of null.
+	 */
+	public void testAddAndDeleteRecipes() {
+		// First delete all the recipes from previous tests
+		int numRecipes = cm.getRecipes().length;
+		for(int i=0; i<numRecipes; i++) {
+			cm.deleteRecipe(i);
+		}
+		
+		cm.addRecipe(r1); // [r1, null, null, null]
+		cm.addRecipe(r2); // [r1, r2, null, null]
+		cm.deleteRecipe(1); // [r1, null, null, null]
+		assertEquals("Coffee", cm.getRecipes()[0].getName());
+		cm.addRecipe(r3); // [r1, r3, null, null]
+		cm.addRecipe(r2); // [r1, r3, r2, null]
+		cm.deleteRecipe(0); // [null, r3, r2, null]
+		assertEquals("Latte", cm.getRecipes()[1].getName());
+		cm.addRecipe(r1); // [r1, r3, r2, null]
+		cm.addRecipe(r4); // [r1, r3, r2, r4]
+		assertEquals("Hot Chocolate", cm.getRecipes()[3].getName());
 	}
 
 }
